c5841a1c154d433a00b66d40bba1e9acca77d51d
Ayan Gorain <100009409+ayan-gorain@users.noreply.github.com>
2025-01-13 16:11:13 +0530

    ggg

---
+++ basicmaths/counttheno.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    
+    int count=0;
+    int dig=x%10;
+    
+    x=x/10;
+    
+
+    cout<<count;
+
+
+  return 0;
+}
